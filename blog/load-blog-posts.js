c4265c6fae8fcc0874928c13a0482eae8cfab160
mcne0004 <mcne0004@hz.nl>
2025-09-10 16:11:30 +0200

    website requirements finished

--- blog/load-blog-posts.js
+++ blog/load-blog-posts.js
@@ -1,5 +1,29 @@
-let pagesize = 5
-let latestpost = 10
+let pagesize = 20
+let latestpost = 4
 let page = 1
 let pagestoload = Array(pagesize).fill().map((_, index) => -index + latestpost - (page - 1) * pagesize).filter(function(x){ return x > 0 })
-console.log(pagestoload)
+
+async function loadBlogPosts() {
+    for (item of pagestoload) {
+        const file = await fetch(window.location.origin + "/blog/" + item.toString() + ".html")
+        const text = await file.text()
+
+        let blogpost = new DOMParser().parseFromString(text, "text/html")
+        let title = blogpost.getElementById("title").innerHTML
+        let date = blogpost.getElementById("date").innerHTML
+        let description = blogpost.getElementById("description").innerHTML
+
+        const article = await fetch(window.location.origin + "/blog/article.html")
+        const articletext = await article.text()
+
+        let articlehtml = new DOMParser().parseFromString(articletext, "text/html")
+        articlehtml.getElementsByClassName("title")[0].innerHTML = title
+        articlehtml.getElementsByClassName("date")[0].innerHTML = date
+        articlehtml.getElementsByClassName("description")[0].innerHTML = description
+        articlehtml.getElementsByClassName("articlelink")[0].setAttribute("href", window.location.origin + "/blog/" + item.toString() + ".html")
+
+        document.getElementById("articles").innerHTML += articlehtml.documentElement.innerHTML
+    }
+}
+
+loadBlogPosts()
--- generic/js/load-nav-menu.js
+++ generic/js/load-nav-menu.js
@@ -3,6 +3,8 @@ async function loadNavMenu() {
     const text = await file.text()
 
     document.getElementById("nav-menu").innerHTML = text
+    let findcurrent = document.evaluate("//a[contains(., '" + document.title + "')]", document, null, XPathResult.ANY_TYPE, null)
+    findcurrent.iterateNext().style.backgroundColor = "#303030"
 }
 
 loadNavMenu()
