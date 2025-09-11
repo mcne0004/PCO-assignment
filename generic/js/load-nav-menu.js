739cf319487cfbea28694c5c8ee88012921f3d1a
mcne0004 <mcne0004@hz.nl>
2025-09-11 09:23:02 +0200

    fix 404 problem on github pages for nav-menu

--- generic/js/load-nav-menu.js
+++ generic/js/load-nav-menu.js
@@ -1,5 +1,5 @@
 async function loadNavMenu() {
-    const file = await fetch(window.location.origin + "/generic/html/nav-menu.html")
+    const file = await fetch("../html/nav-menu.html")
     const text = await file.text()
 
     document.getElementById("nav-menu").innerHTML = text
